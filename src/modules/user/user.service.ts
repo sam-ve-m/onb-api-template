c767c32cc12efad0f2bd1dc00a4b15aea3258c7b
Luiz Soares <luiz.soares@euqueroinvestir.com>
2023-03-06 14:20:09 -0300

    test: fix unit test to user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -8,23 +8,22 @@ export class UserService {
   constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
 
   async findAll(): Promise<User[]> {
-    return await this.userModel.find().exec();
+    return this.userModel.find();
   }
 
   async findById(id: string): Promise<User> {
-    return await this.userModel.findById(id).exec();
+    return this.userModel.findById(id);
   }
 
   async create(user: User): Promise<User> {
-    const createdUser = new this.userModel(user);
-    return await createdUser.save();
+    return this.userModel.create(user);
   }
 
   async update(id: string, user: User): Promise<User> {
-    return await this.userModel.findByIdAndUpdate(id, user, { new: true });
+    return this.userModel.findByIdAndUpdate(id, user, { new: true });
   }
 
   async delete(id: string): Promise<User> {
-    return await this.userModel.findByIdAndRemove(id);
+    return this.userModel.findByIdAndRemove(id);
   }
 }
--- test/modules/user/user.service.spec.ts
+++ test/modules/user/user.service.spec.ts
@@ -1,42 +1,35 @@
-// users.service.spec.ts
-
+import { UserService } from './../../../src/modules/user/user.service';
 import { Test, TestingModule } from '@nestjs/testing';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { UserService } from '../../../src/modules/user/user.service';
+import { getModelToken } from '@nestjs/mongoose';
 import { User } from '../../../src/modules/user/user.model';
-import { UserSchema } from '../../../src/modules/user/user.schema';
 
 describe('UserService', () => {
   let service: UserService;
-  let module: TestingModule;
+  const userModel = {
+    create: jest.fn(),
+    find: jest.fn(),
+  };
 
-  beforeAll(async () => {
-    module = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot(),
-        MongooseModule.forRootAsync({
-          useFactory: async (configService: ConfigService) => ({
-            uri: `mongodb://${configService.get('DB_HOST')}:${configService.get('DB_PORT')}/${configService.get(
-              'DB_NAME',
-            )}`,
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true,
-          }),
-          inject: [ConfigService],
-        }),
-        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
+      providers: [
+        UserService,
+        {
+          provide: getModelToken(User.name),
+          useValue: userModel,
+        },
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
       ],
-      providers: [UserService],
     }).compile();
 
     service = module.get<UserService>(UserService);
-  });
-
-  afterAll(async () => {
-    await module.close();
   });
 
   it('should be defined', () => {
@@ -44,21 +37,33 @@ describe('UserService', () => {
   });
 
   it('should create a user', async () => {
-    const createdUser = await service.create({
+    const user = {
       name: 'fake_name',
       password: 'fake_password',
       email: 'fake@example.com',
-    });
+    };
+    userModel.create.mockReturnValue(user);
 
-    expect(createdUser).toHaveProperty('id');
+    const createdUser = await service.create(user);
+
     expect(createdUser.name).toBe('fake_name');
     expect(createdUser.password).toBe('fake_password');
     expect(createdUser.email).toBe('fake@example.com');
   });
 
   it('should find all users', async () => {
+    const user = {
+      name: 'fake_name',
+      password: 'fake_password',
+      email: 'fake@example.com',
+    };
+    userModel.find.mockReturnValue([user]);
+
     const users = await service.findAll();
 
     expect(users).toHaveLength(1);
+    expect(users[0].name).toBe('fake_name');
+    expect(users[0].password).toBe('fake_password');
+    expect(users[0].email).toBe('fake@example.com');
   });
 });
